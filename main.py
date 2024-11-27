ca9b78b938dddc1edfd76f944733cb41352b4a30
psrinathmanoj <ps.manoj@ymail.com>
2024-11-27 07:59:03 +0530

    added yfinance to get stock data

---
+++ main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+import yfinance as yf
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello World"}
+
+# fast api to get all the stock data from yahoo finance package
+@app.get("/stock")
+def get_stock_data(symbol: str):
+    stock_data = yf.Ticker(symbol)
+    return stock_data.history(period="1d", interval="1m")
